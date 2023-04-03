ca8bf63ab16814293074a07eba75375b0ad1c2b4
Michael Osei-Bobie <michael.osei-bobie@turntabl.io>
2023-04-03 16:29:10 +0000

    code structure

---
+++ src/main/java/org/tlc6/rhumemob/enums/CardSuits.java
@@ -0,0 +1,8 @@
+package org.tlc6.rhumemob.enums;
+
+public enum CardSuits {
+    HEARTS,
+    DIAMONDS,
+    CLUBS,
+    SPADES;
+}
---
+++ src/main/java/org/tlc6/rhumemob/models/CardDeck.java
@@ -0,0 +1,4 @@
+package org.tlc6.rhumemob.models;
+
+public class CardDeck {
+}
